0ce067e5801fead2dce5ee65618879c91c4f6120
Matt Schorr <matthew@coffeegem.com>
2024-09-10 21:50:55 -0700

    Update appp-3.py

--- appp-3.py
+++ appp-3.py
@@ -979,8 +979,8 @@ if uploaded_file_sales is not None and uploaded_file_customer is not None:
     )
     
     # Display the plot in Streamlit
-    st.markdown(":white[Hello There]")
-    st.markdown(":white[YOU WERE MEANT TO DESTROY THE SITH NOT JOIN THEM!]")
+    st.markdown(" ")
+    st.markdown(" ")
     st.markdown("**What Makes a VIP Customer? Lets use Logistic Regression to find out**")
     st.markdown(":blue[A VIP returns often and spends a lot (M + F > 5)]")
     st.plotly_chart(fig, use_container_width=True)

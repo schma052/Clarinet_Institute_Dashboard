7524181fd3a2c2113a7c53843a31874600829e1e
Matt Schorr <matthew@coffeegem.com>
2024-09-24 09:55:28 -0700

    Update appp-3.py

--- appp-3.py
+++ appp-3.py
@@ -1577,7 +1577,7 @@ if uploaded_file_sales is not None and uploaded_file_customer is not None:
 
     # Sort data first by item and then by date
     digi_df.sort_values(by=['Date', 'Email Unsub', 'Country'], inplace=True)
-    digi_df = digi_df.groupby('Date', 'Country').sum()
+    digi_df = digi_df.groupby('Date').sum()
     st.markdown("**Daily Email Unsubscriptions:**")
     st.line_chart(digi_df[['Email Unsub', color == 'Country']])
 
